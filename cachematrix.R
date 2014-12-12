fc37ae88a55977dc76c6bf7a865a8e9d13c57974
luisandrecunha <luisandrecunha@gmail.com>
2014-12-12 17:09:43 +0000

    Implementation Conclusion
    Creation of cacheSolve.
    Creation of test scripts

--- cachematrix.R
+++ cachematrix.R
@@ -2,22 +2,35 @@
 ##
 ## Luís Cunha
 ## 
-## Write a short comment describing this functionhy
+
+
+# This function is used to create a cached inverse matrix. The function have 4 
+# functions that allows to:
+# - add (setMatrix) the matrix to be inverted
+# - get (getMatrix) the value of the matrix to be inverted (matrixEnvironment0) 
+#       if exists
+# - recalculate (setInverse) the inverse if it is a new matrix
+# - get (getInverse) the value of its inverse (inverseEnvironment0)
 
 makeCacheMatrix <- function(matrixEnvironment0 = matrix()) {
+        
         inverseEnvironment0 <- NULL
         
-        setMatrix <- function(matrixToSet) { ## It will load the matrix to other environment
+        # add (setMatrix) the matrix to be inverted
+        setMatrix <- function(matrixToSet) { 
                 matrixEnvironment1 <<- matrixToSet
                 inverseEnvironment1 <<- NULL
-                
         }
         
+        # - get (getMatrix) the value of the matrix to be inverted 
+        #       (matrixEnvironment0) if exists
         getMatrix <- function() matrixEnvironment1 ## It will get the matrix if it exists from the other environment
         
-        setInverse <- function(solve) inverseEnvironment1 << solve
+        # - recalculate (setInverse) the inverse if it is a new matrix
+        setInverse <- function(solve) inverseEnvironment1 <<- solve
         
-        getInverse <- function() inverserEnvironment1
+        # - get (getInverse) the value of its inverse (inverseEnvironment0)
+        getInverse <- function() inverseEnvironment1
         
         list (
                 setMatrix = setMatrix,
@@ -28,8 +41,45 @@ makeCacheMatrix <- function(matrixEnvironment0 = matrix()) {
 }
 
 
-## Write a short comment describing this function
+# This function returns the inverse of a matrix that was created with
+# the previous function (makeCacheMatrix). 
+# 
+# If the inverse of the matrix has not been calculated or the original matrix 
+# has changed, then it will recalculate it's inverse. Otherwise it will
+# directly return the cached value of the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        inverseEnvironment0 <- x$getInverse()
+        
+        if (!is.null(inverseEnvironment0)) {
+                message("getting cached inverse")
+                return(inverseEnvironment0)
+        }
+        else {
+                message("new Matrix")
+                data <- x$getMatrix()
+                inverseEnvironment0 <- solve(data)
+                x$setInverse(inverseEnvironment0)
+                return(inverseEnvironment0)
+        }
+        
 }
+
+##
+## Test scripts
+##
+
+# Create and load the Matrix
+matrix <- makeCacheMatrix()
+matrix$setMatrix(matrix( c(1,1,1,3,4,3,3,3,4), nrow=3, ncol=3))
+
+# First time, cacheSolve() computes the inverse
+cacheSolve(matrix)
+
+# Second time, cacheSolve() returns the cached inverse
+cacheSolve(A)
+
+# New matrix, cacheSolve() recomputes the inverse
+matrix$setMatrix(matrix( c(7,-1,-1,-3,1,0,-3,0,1), nrow=3, ncol=3))
+cacheSolve(matrix)
